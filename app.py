af3b1c1cd054716acc48b67ea221a6346681a9b7
daniyalrosli <94339715+daniyalrosli@users.noreply.github.com>
2024-10-21 23:25:39 +0800

    add model

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = pickle.load(open('models/logistic_regression_model.pkl', 'rb'))
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    features = [data['age'], data['sex'], data['cp'], data['trestbps'], data['chol'], 
+                data['fbs'], data['restecg'], data['thalach'], data['exang'], 
+                data['oldpeak'], data['slope'], data['ca'], data['thal']]
+    prediction = model.predict([np.array(features)])
+    output = prediction[0]
+    return jsonify({'prediction': int(output)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
